f072b41a4f5664c6f87a0c6281a3e90d8aabc6cf
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2019-04-23 18:45:14 -0500

    Adding react started

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 // DB Config
 const db = require('./config/keys').mongoURI
 const routerApi = require('./api/router');

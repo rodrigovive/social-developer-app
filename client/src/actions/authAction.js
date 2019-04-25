fad3d5155563180c033905a6a1a8eb3ea3ab3041
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2019-04-25 12:17:24 -0500

    Redux Login Action and set current user

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,12 +1,13 @@
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
 // Register User
-
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
-    .then(res => history.push('/login'))
+    .then(res => history.push("/login"))
     .catch(err => {
       dispatch({
         type: GET_ERRORS,
@@ -14,3 +15,33 @@ export const registerUser = (userData, history) => dispatch => {
       });
     });
 };
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // Save to LocalStorage
+      const { token } = res.data;
+      // Set token to LocalStorage
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth Header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err => {
+      const { errors } = err.response.data;
+      console.log(errors);
+    });
+};
+
+export const setCurrentUser = (decoded) => {
+
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { SET_CURRENT_USER } from "../actions/types";
+import isEmpty from "lodash/isEmpty";
 const initialState = {
   isAuthenticate: false,
   user: {}
@@ -5,7 +7,13 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-   
+    case SET_CURRENT_USER: {
+      return {
+        ...state,
+        isAuthenticate: !isEmpty(action.payload),
+        user: action.payload
+      };
+    }
     default:
       return state;
   }

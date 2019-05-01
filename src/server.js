2bd7144a7f4be49326466b485d5a83b9ab4938a7
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2019-04-30 21:46:40 -0500

    Fix Static Folder

--- src/server.js
+++ src/server.js
@@ -22,9 +22,10 @@ app.use("/api", routerApi);
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.resolve(__dirname, "../client", "build")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 

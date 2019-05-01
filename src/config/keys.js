633866a36cc594d3eed4de2076ac0f949331b2a0
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2019-04-30 19:49:32 -0500

    Preparing for deploy

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://root:viveros2@ds121026.mlab.com:21026/deconnector',
-  secretOrKey: 'secret'
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
 }
--- src/server.js
+++ src/server.js
@@ -1,31 +1,38 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const app = express();
+const port = process.env.PORT || 5000;
 // DB Config
-const db = require('./config/keys').mongoURI
-const routerApi = require('./api/router');
-
+const db = require("./config/keys").mongoURI;
+const routerApi = require("./api/router");
+const path = require("path");
 // Body parser middleware
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Passport middleware
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport.js')(passport);
+require("./config/passport.js")(passport);
 
-app.use('/api',routerApi)
+app.use("/api", routerApi);
 
-mongoose.connect(db)
-.then(() => console.log('MONGODB CONNECTED'))
-.catch(err => console.log(err))
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
+mongoose
+  .connect(db)
+  .then(() => console.log("MONGODB CONNECTED"))
+  .catch(err => console.log(err));
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
-
+  console.log(`Server running on port ${port}`);
+});

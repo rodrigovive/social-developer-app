00ec5641e9b7d6f66be9b6ba4ddb99a2488c4ed1
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2019-05-03 23:39:06 -0500

    Fix deploy in heroku

--- src/server.js
+++ src/server.js
@@ -22,11 +22,12 @@ app.use("/api", routerApi);
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.resolve(__dirname, "../client", "build")));
+  app.use(express.static(path.join(__dirname, "../client", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
+  console.log('production',path.join(__dirname, "../client", "build"));
 }
 
 mongoose
